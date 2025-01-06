51aeed7cc7fb02e22c6370f029ee0892780bc6f1
Anna Vesely <53512166+annavesely@users.noreply.github.com>
2025-01-06 20:21:27 +0100

    Aggiunte reference a documentazione pima-package

--- R/pima_package.R
+++ R/pima_package.R
@@ -15,6 +15,13 @@
 #' @examples
 #' # COPIARE ESEMPIO DA FUNZIONE PIMA
 #' 
+#' @references
+#' Girardi, Vesely, Lakens, Altoè, Pastore, Calcagnì, Finos (2024). Post-selection Inference in Multiverse Analysis (PIMA): An Inferential Framework Based on the Sign Flipping Score Test. Psychometrika, doi: 10.1007/s11336-024-09973-6.
+#' 
+#' Hemerik, Goeman, Finos (2020). Robust testing in generalized linear models by sign flipping score contributions. Journal of the Royal Statistical Society, Series B (Statistical Methodology), doi: 10.1111/rssb.12369.
+#' 
+#' De Santis, Goeman, Hemerik, Davenport, Finos (2024). Inference in generalized linear models with robustness to misspecified variances. Pre-print, doi: 10.48550/arXiv.2209.13918.
+#' 
 # @docType _PACKAGE
 #' @author Livio Finos
 #' @name pima-package
