34f0b733c2b20e6845bda005d52c029dae5d8d5b
Anna Vesely <53512166+annavesely@users.noreply.github.com>
2025-01-22 13:39:28 +0100

    Funzione global_tests(): sistemati link

--- R/global_test.R
+++ R/global_test.R
@@ -1,10 +1,11 @@
 #' Global tests in multiverse analysis
 #' @description Tests global null hypotheses for weak FWER control, considering one or more parameters within a multiverse of models.
 #' @usage global_tests(obj, comb_funct = "maxT", by = NULL) 
-#' @param obj a \code{jointest} object, e.g., produced by \code{\link[pima]{pima}}.
-#' @param comb_funct combining function (see \code{\link[combine-methods]{combine-methods}} for more datails).
+#' @param obj a \code{jointest} object, e.g., produced by \code{\link{pima}}.
+#' @param comb_funct combining function (see \code{\link[jointest]{combine-methods}} for more datails).
 #' @param by name of the column of \code{summary_table} used for combining.
 #' If \code{NULL} (default), a single global null hypothesis is tested.
+#' @details This function builds on the function \code{combine} of the \strong{jointest} package.
 #' @details In the default \code{by = NULL}, the procedure tests the global null hypothesis
 #' that there are no significant effects (all considered coefficients in all models are null)
 #' against the alternative that there is at least one significant effect
