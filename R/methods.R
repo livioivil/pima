c676450db598f53d37bb3143abfa1485ce0cb5d4
filippogambarota <filippo.gambarota@gmail.com>
2025-10-10 11:34:18 +0200

    cleaning methods

--- R/methods.R
+++ R/methods.R
@@ -45,8 +45,6 @@ summary.pima <- function(object, ...) {
   tab = cbind(Coeff = rownames(tab), tab)
 }
 
-
-is_signif = NULL
 ###########################
 #' as.pima method for a pima object.
 #' @rdname pima-method
@@ -73,7 +71,7 @@ as.pima <- function(object, names_obj = NULL, ...) {
 #' plot.pima summary method for a pima object.
 #' @rdname pima-method
 #' @param object an object of class \code{pima}.
-#' @param p.values  use "raw" or "adjusted" (i.e. corrected for selective inference)
+#' @param p.values  use "raw" or "adjusted" (i.e. corrected for selective inference). Default to "raw".
 #' @param y `"-log10(p)"` by default. It can be any column's name of the `summary()` of a `pima-object` (e.g. `"z value"`) or any other expression e.g. `"-log10(p)"`.
 #' @param x `"Estimate"` by default. It can be any column's name of the `summary()` of a `pima-object` (e.g. `"Part. Cor"`) or any other expression e.g. `"-log10(p)"`.
 #' @param alpha a value between 0 and 1. The plot will mark the p-values smaller than \code{alpha} (0.05 by default). If equal to 0 or 1 nothing will be marked.
@@ -81,79 +79,100 @@ as.pima <- function(object, names_obj = NULL, ...) {
 #' @method  plot pima
 #' @docType methods
 #' @export
-#' @import ggplot2
 
-plot.pima <-
-  function(
-    object,
-    p.values = c("adjusted", "raw"),
+plot.pima <- function(
+    object, 
+    p.values = "adjusted",
     alpha = .05,
-    y = "-log10(p)",
-    x = "Estimate"
-  ) {
-    p.values = p.values[1]
-    group = "Coeff"
-    D = object$summary_table
-    D$.assign = NULL
-    colnames(D) = gsub(" ", "", colnames(D))
-    x = x[1] #match.arg(x[1],c("Estimate","Part. Cor"))
-    y = y[1] #match.arg(y[1], c("-log10(p)","z value"))
-    x = gsub(" ", "", x)
-    y = gsub(" ", "", y)
-    #
-    if (p.values == "raw") {
-      D$is_signif = (D$p <= alpha)
-    } else {
-      #adj
-      adj_id = grep("p.adj", colnames(D))[1]
-      D$is_signif = (D[, adj_id] <= alpha)
-    }
-
-    if (p.values == "raw") {
-      title = "(Raw) p-values"
-    } else {
-      title = "Adjusted p-values"
-    }
-
-    p <- ggplot(D, aes_string(y = y, x = x, group = group, color = group)) +
-      geom_point(aes(shape = is_signif), size = 2) + #is_signif))+
-      ggtitle(title) +
-      theme_minimal()
-    if (!(alpha %in% c(0, 1))) {
-      p <- p +
-        scale_shape_manual(
-          values = c(3, 19),
-          name = paste(p.values, "p-value"),
-          labels = paste0(c("p >  ", "p <= "), alpha)
-        )
-    }
-    #RIRR and RP indices
-    IRR <- exp(object$summary_table$Estimate)
-    RIRR <- as.numeric(quantile(IRR, c(0.99)) / quantile(IRR, c(0.01)))
-    #Relative pvalues
-    RP_raw <- as.numeric(diff(
-      -log10(quantile(object$summary_table$p, c(0.99, 0.01)))
-    ))
-    RP_adjusted <- as.numeric(diff(
-      -log10(quantile(object$summary_table$p.adj, c(0.99, 0.01)))
-    ))
-    if (p.values == "raw") {
-      indices <- paste(
-        "RIRR=",
-        round(RIRR, 3),
-        "\nRP=",
-        round(RP_raw, 3),
-        sep = ""
-      )
-    }
-    if (p.values == "adjusted") {
-      indices <- paste(
-        "RIRR=",
-        round(RIRR, 3),
-        "\nRP=",
-        round(RP_adjusted, 3),
-        sep = ""
-      )
-    }
-    p
+    xvar = NULL,
+    yvar = NULL,
+    ...
+  )
+  {
+  
+  # TODO what about adding a way to transform the p value using custom formula? Also adding critical value (p = 0.05) when using a transformation.
+  
+  # avoid conflicting with base plot(x = ) argument
+  
+  if(is.null(xvar)) xvar <- "Estimate"
+  if(is.null(yvar)) yvar <- "-log10(p)"
+  
+  D = object$summary_table
+  D$.assign = NULL
+  
+  # check if is one of the available columns
+  xvar <- match.arg(xvar, choices = colnames(D), several.ok = FALSE)
+  
+  # for yvar the name can be also an expression (as character) to be
+  # evaluated. Thus check if y exist, otherwise try to evaluate the expression
+  
+  if(!yvar %in% colnames(D)){
+    D[[yvar]] <- eval(parse(text = yvar), envir = D)
   }
+  
+  p.values = match.arg(p.values, choices = c("adjusted", "raw"))
+  group = "Coeff"
+
+  # RIRR and RP indices
+  
+  IRR <- exp(object$summary_table$Estimate)
+  RIRR <- as.numeric(quantile(IRR, c(0.99)) / quantile(IRR, c(0.01)))
+  
+  # Relative pvalues
+  RP_raw <- as.numeric(diff(
+    -log10(quantile(object$summary_table$p, c(0.99, 0.01)))
+  ))
+  
+  RP_adjusted <- as.numeric(diff(
+    -log10(quantile(object$summary_table$p.adj, c(0.99, 0.01)))
+  ))
+  
+  if (p.values == "raw") {
+    D$is_signif = (D$p <= alpha)
+    title = "(Raw) p-values"
+    
+    # TODO how to use thr RIIR and RP indexes?
+    
+    indices <- paste(
+      "RIRR=",
+      round(RIRR, 3),
+      "\nRP=",
+      round(RP_raw, 3),
+      sep = ""
+    )
+    
+  } else {
+    # adj
+    title = "Adjusted p-values"
+    adj_id = grep("p.adj", colnames(D))[1]
+    D$is_signif = (D[, adj_id] <= alpha)
+    
+    indices <- paste(
+      "RIRR=",
+      round(RIRR, 3),
+      "\nRP=",
+      round(RP_adjusted, 3),
+      sep = ""
+    )
+    
+  }
+
+  p <- ggplot2::ggplot(D, 
+               ggplot2::aes(x = .data[[xvar]], 
+                  y = .data[[yvar]], 
+                  group = .data[[group]], 
+                  color = .data[[group]])) +
+    ggplot2::geom_point(ggplot2::aes(shape = is_signif), size = 2) +
+    ggplot2::ggtitle(title) +
+    ggplot2::theme_minimal()
+  
+  if (!(alpha %in% c(0, 1))) {
+    p <- p +
+      ggplot2::scale_shape_manual(
+        values = c(3, 19),
+        name = paste(p.values, "p-value"),
+        labels = paste0(c("p >  ", "p <= "), alpha)
+      )
+  }
+  p
+}
