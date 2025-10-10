0ab70f299af9693f7b86974b52111dc6177b4d94
filippogambarota <filippo.gambarota@gmail.com>
2025-10-10 11:35:12 +0200

    new specification curve

--- R/spec_curve.R
+++ R/spec_curve.R
@@ -2,116 +2,112 @@
 #'
 #' This function performs a specification curve analysis based on the results of a set of regression models. It visualizes the coefficient estimates with confidence intervals, p-values, and highlights significant specifications.
 #'
-#' @param res A list object with regression model results, including `mods` (a list of models) and `summary_table` (a data frame containing estimates and p-values).
+#' @param x an object of class `pima`, usually the result of the `pima()` function.
+#' @param yvar character indicating the column of the `x$summary_table` object to be used in the y axes of the top part of the specification curve (usually the estimated parameter). Default to `"Estimate"`.
+#' @param p.values character indicating the column of the `x$summary_table` object with the p values. Default to `""p.adj.maxT"` (maxT corrected p values). 
 #' @param alpha A numeric value specifying the significance level for the confidence intervals. Default is 0.05.
-#' @param p.values A char string indicating which type of p-values to use. Options are `"raw"` or `"adjusted"` (default). In both cases, they are taken from the `summary_table`.
-#'
-#' @return A plot displaying the specification curve with confidence intervals and p-values, as well as a legend showing the variable combinations used in each specification.
-#'
-#' @import ggplot2
-#' @import dplyr
-#' @import cowplot
+#' @return A plot displaying the specification curve with confidence intervals and p-values, as well as a legend showing the variable combinations used in each specification. The output object is a [`patchwork`] object thus a collection of `ggplot2` objects. The underlying datasets can be accessed using `@data` for each plot.
+#' @param tbr A numeric vector of two elements indicating the proportion of space assigned to the top and bottom part of the plot.
+#' @param colors A character vector of two elements with the colors for the point representing non-significant and significant p-values.
+#' @param A character vector of two elements with the shapes representing non-significant and significant p-values. Default to simple points.
+#' @title A character vector with the title for the overall plot (applied to the top plot internally).
+#' @param xlab a character vector for the x axis title. Default to "Specification"
+#' @param ylab a character vector for the y axis title of the top plot. Default to `yvar`.
+#' @param top.theme a function with a `ggplot2` compatible theme for the top plot. Default to ggplot2::theme_minimal()`
+#' @param bottom.theme a function with a `ggplot2` compatible theme for the bottom plot Default to `ggplot2::theme_minimal()`
 #' @export
 #'
 #' @examples
 #' # Example usage (assuming `res` is a pre-computed result object):
 #' # spec_curve(res, alpha = 0.05)
 #' 
-spec_curve <- function(res, alpha = 0.05,p.values=c("adjusted","raw")) {
-  # Extract data from res
-  data_ori <- res$mods[[1]]$data
-  cmb <- names(res$mods)
-  n_spec <- length(res$mods)
-  num_var_test<-length(res$tested_coeffs)
-  # if(num_var_test>1){stop("The number of tested coefficients is higher 1")}
+spec_curve <- function(x,
+                       focal = NULL,
+                       yvar = "Estimate",
+                       p.values = "p.adj.maxT",
+                       alpha = 0.05,
+                       tbr = c(0.4, 0.6),
+                       colors = NULL,
+                       shapes = NULL,
+                       title = NULL,
+                       xlab = NULL,
+                       ylab = NULL,
+                       top.theme = NULL,
+                       bottom.theme = NULL) {
   
-  # Calculate approximate confidence intervals 
-  if(p.values=="raw"){
-    z_value <- qnorm(1 - alpha / 2)
-    #if(num_var_test>1) coeff_estimates <- res$summary_table$`Part. Cor` else 
-    coeff_estimates <- res$summary_table$Estimate
-    std_devs<-res$summary_table$Estimate/qnorm(1-res$summary_table$p/2)
-    lower_ci <- coeff_estimates - z_value * std_devs
-    upper_ci <- coeff_estimates + z_value * std_devs
-    p<-res$summary_table$p
+  stopifnot(inherits(x, "pima"))
+  
+  if(is.null(title)){
+    title <- sprintf("Specification Curve (n = %s scenarios)", nrow(x$info))
   }
-  # Calculate approximate confidence intervals
-  if(p.values=="adjusted"){
-    z_value <- qnorm(1 - alpha / 2)
-    #res$summary_table$p.adj<- maxT.light(res$Tspace,exp(seq(-8,0,0.5)))
-    #if(num_var_test>1) coeff_estimates <- res$summary_table$`Part. Cor` else 
-    coeff_estimates <- res$summary_table$Estimate
-    std_devs<-res$summary_table$Estimate/qnorm(1-res$summary_table$p.adj/2)
-    lower_ci <- coeff_estimates - z_value * std_devs
-    upper_ci <- coeff_estimates + z_value * std_devs
-    p<-res$summary_table$p
-    p.adj<-res$summary_table$p.adj
+  
+  if(is.null(colors)){
+    colors <- c(scales::alpha("black", 0.5), "firebrick")
   }
-  # Prepare data for plotting
-  if(p.values=="raw"){
-    df <- data.frame(
-      SpecID = rank(coeff_estimates),
-      Coefficients = coeff_estimates,
-      StdDev = std_devs,
-      PValue = p,
-      LowerCI = lower_ci,
-      UpperCI = upper_ci,
-      Significance = ifelse(p < alpha, "Yes","No")
-    )
+  
+  if(is.null(top.theme)) top.theme <- ggplot2::theme_minimal
+  if(is.null(bottom.theme)) bottom.theme <- ggplot2::theme_minimal
+  
+  yvar <- match.arg(yvar, choices = colnames(x$summary_table), several.ok = FALSE)
+  p.values <- match.arg(p.values, choices = colnames(x$summary_table), several.ok = FALSE)
+  
+  if(is.null(xlab)) xlab <- "Specification"
+  if(is.null(ylab)) ylab <- yvar
+  
+  xs <- attributes(x$info)$xs
+  
+  if(is.null(focal) & is.null(x$tested_coeffs)){
+    stop("when no tested_coeffs are indicated in the pima() call, focal cannot be NULL!")
   }
-    if(p.values=="adjusted"){
-      df <- data.frame(
-        SpecID = rank(coeff_estimates),
-        Coefficients = coeff_estimates,
-        StdDev = std_devs,
-        PValue = p,
-        LowerCI = lower_ci,
-        UpperCI = upper_ci,
-        Significance = ifelse(p.adj < alpha, "Yes (adjusted)","No")
-      )
+  
+  if(is.null(focal)){
+    focal <- x$tested_coeffs
+    if(length(focal) > 1){
+      # TODO better handling here of tested_coeffs >1 and focal is null
+      warning("the number of tested_coeffs is greater than 1, the specification curve could be misleading!")
+    }
   }
-  # Plot the specification curve
-  p1 <- ggplot(df, aes(x = SpecID, y = Coefficients)) +
-    geom_point(aes(color = Significance), size = 3) +
-    geom_errorbar(aes(ymin = LowerCI, ymax = UpperCI), width = 0.2) +
-    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-    labs(
-      title = "Specification Curve Analysis",
-      x = "SpecID",
-      y = "Coefficient Estimate",
-      color = paste("p <", alpha)
+  
+  x$summary_table <- subset(x$summary_table, Coeff == focal)
+  spec_data <- .get_spec_curve_data(x, yvar, p.values, alpha)
+  
+  # remove redundant variables from the plot
+  redundant <- apply(x$info[, xs], 2, function(x) length(unique(x)) == 1)
+  redundant <- names(redundant)[redundant]
+  
+  spec_data$dbottom <- subset(spec_data$dbottom, var != redundant)
+  
+  top <- ggplot2::ggplot(data = spec_data$dtop,
+                         ggplot2::aes(x = .id_spec,
+                                      y = .data[[yvar]])) +
+    ggplot2::geom_point(ggplot2::aes(color = is_signif), show.legend = FALSE) +
+    top.theme() +
+    ggplot2::theme(
+      axis.title.x = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.ticks.x = ggplot2::element_blank()
     ) +
-    theme_minimal()
+    ggplot2::ggtitle(title) +
+    ggplot2::scale_color_manual(values = colors) +
+    ggplot2::scale_shape_manual(values = shapes) +
+    ggplot2::labs(
+      y = ylab
+    )
   
-  # Legend of specifications
-  combinations_wide<-res$info
+  bottom <- ggplot2::ggplot(spec_data$dbottom, 
+                            ggplot2::aes(x = .id_spec,
+                                         y = var.spec)) +
+    ggplot2::facet_grid(var ~ ., scales = "free_y") +
+    ggplot2::geom_point() +
+    bottom.theme() +
+    ggplot2::theme(
+      axis.title.y = ggplot2::element_blank()
+    ) +
+    ggplot2::labs(
+      x = xlab
+    )
   
-  combinations_wide$SpecID<-rank(res$summary_table$Estimate)
-  confounders_names<-names(combinations_wide)[(3+num_var_test):(length(combinations_wide)-1)]
-  combinations_long<-reshape(combinations_wide,direction="long",idvar = "SpecID", varying = list((4+num_var_test):(length(combinations_wide)-1)),v.names = "Type")
-  names(combinations_long)[5+num_var_test]<-"Variable"
-  combinations_long$Type[combinations_long$Type=="FALSE"]<-"false("
-  combinations_long$Type[combinations_long$Type=="TRUE"]<-"true("
-  combinations_long$Type[!grepl("([()])",combinations_long$Type)]<-"original"
-  combinations_long$Type[grepl("([()])",combinations_long$Type)]<-sub("\\(.*", "", combinations_long$Type)[grepl("([()])",combinations_long$Type)]
-  combinations_long$Variable<-as.factor(combinations_long$Variable)
-  levels(combinations_long$Variable)<-confounders_names
-  p2 <- ggplot(combinations_long, aes(x = Variable, y = SpecID, col = Type)) +
-    geom_point(aes(color = Type), shape = 1, size = 4, position = position_dodge(width = 0.4)) +
-    theme_bw() + coord_flip()
+  patchwork:::`/.ggplot`(top, bottom) +
+    patchwork::plot_layout(heights = tbr)
   
-  legend <- cowplot::get_plot_component(p1, "guide-box", return_all = TRUE)[[1]]
-  legend1 <- cowplot::get_plot_component(p2, "guide-box", return_all = TRUE)[[1]]
-  combineLegend <- cowplot::plot_grid(legend, legend1, nrow = 2)
-  
-  # Combine plots into a grid
-  prow <- cowplot::plot_grid(p1 + theme(legend.position = "none"),
-                    p2 + theme(legend.position = "none"),
-                    align = 'v',
-                    labels = c("A", "B"),
-                    hjust = -1,
-                    nrow = 2)
-  p <- cowplot::plot_grid(prow, combineLegend, rel_widths = c(3, 0.3))
-  
-  return(p)
 }
