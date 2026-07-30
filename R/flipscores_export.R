78c190c812845446d928603f66c94b0d5a9251d4
Filippo Gambarota <filippo.gambarota@gmail.com>
2026-07-30 14:12:22 +0200

    adding flipscores exports

---
+++ R/flipscores_export.R
@@ -0,0 +1,11 @@
+#' @export
+#' @importFrom flipscores combine_tests
+flipscores::combine_tests
+
+#' @export
+#' @importFrom flipscores combine_contrasts
+flipscores::combine_contrasts
+
+#' @export
+#' @importFrom flipscores p.adjust
+flipscores::p.adjust
