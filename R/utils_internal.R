c057134c1b3331ba969b41902ad49a6337caeff2
Filippo Gambarota <filippo.gambarota@gmail.com>
2026-07-30 14:57:36 +0200

    remove old utils

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -1,67 +1,3 @@
-.get_all_Tspace <- function(mods) {
-  tab = lapply(mods, function(x) {
-    if (is.null(x$Tspace)) {
-      stop("At least one Tspace is missing")
-    }
-    x$Tspace
-  })
-  nms = sapply(tab, colnames)
-  tab = do.call(cbind, tab)
-  colnames(tab) = nms
-  if (is.null(colnames(tab))) {
-    colnames(tab) = paste0("S", 1:ncol(tab))
-  }
-
-  tab
-}
-
-.get_all_summary_table <- function(mods) {
-  res = lapply(1:length(mods), function(i) {
-    cbind(model = names(mods)[i], mods[[i]]$summary_table)
-  })
-  res = do.call(rbind, res)
-  rownames(res) = NULL
-  res
-}
-
-.get_all_tested_coeffs_names <- function(mods) {
-  sapply(mods, function(md) colnames(md$Tspace))
-}
-
-.get_all_coeff_names_vect <- function(mods) {
-  sapply(mods, function(md) {
-    names(md$coefficients)
-  })
-}
-
-.get_all_coeff_names_list <- function(mods) {
-  lapply(mods, function(md) {
-    names(md$coefficients)
-  })
-}
-
-.get_all_sumScore2 <- function(mods) {
-  sapply(mods, function(md) {
-    sum(md$scores^2)
-  })
-}
-
-.set_mods_names <- function(mods, force = FALSE) {
-  if ((is.null(names(mods))) | force) {
-    paste0("model", 1:length(mods))
-  } else {
-    names(mods)
-  }
-}
-
-.set_comb_names_in_Tspace <- function(combs, comb_names) {
-  res = lapply(1:length(combs), function(i) {
-    colnames(combs[[i]]$Tspace) = comb_names[i]
-    combs[[i]]
-  })
-  res
-}
-
 .trim <- function(x, n = 4) {
   k <- nrow(x)
   if (k <= 4) {
@@ -164,16 +100,6 @@
   Reduce(paste, deparse(x$formula))
 }
 
-.get_formula_x <- function(x) {
-  # with formula tools
-  formula.tools::rhs.vars(x)
-}
-
-.get_formula_y <- function(x) {
-  # with formula tools
-  formula.tools::lhs.vars(x)
-}
-
 # .get_x_name <- function(x) {
 #   attr(x$terms, which = "term.labels")
 # }
@@ -226,19 +152,22 @@ clapply <- function(x, class, FUN, ...) {
 
 # TODO check if better using a different approach
 as.glm <- function(x) {
-  if (inherits(x, "lm")) {
-    call <- getCall(x)
-    call_as_list <- as.list(call)
-    # lm to glm
-    call_as_list[[1]] <- quote(glm)
-    eval(as.call(call_as_list), envir = environment(formula(x)))
-  } else {
-    x
+  if (inherits(x, "glm")) {
+    return(x)
   }
-}
-
-.get_response_var <- function(x) {
-  as.character(x$formula[[2]])
+  
+  if (!inherits(x, "lm")) {
+    return(x)
+  }
+  
+  cl <- getCall(x)
+  cl[[1L]] <- quote(stats::glm)
+  
+  if (is.null(cl$family)) {
+    cl$family <- quote(stats::gaussian())
+  }
+  
+  eval(cl, envir = environment(stats::formula(x)))
 }
 
 .get_family <- function(x){
