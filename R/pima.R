0370add9373349b8aae505443a6e26f956cbcb6d
Filippo Gambarota <filippo.gambarota@gmail.com>
2026-08-07 18:23:53 +0200

    remove temporary model column (now in flipscores)

--- R/pima.R
+++ R/pima.R
@@ -112,9 +112,6 @@ pima <- function(mods, tested_coeffs = NULL, n_flips = 5000, method = c("maxT", 
   )
   
   out <- do.call(flipscores::flipscores, flipscores_args)
-  
-  # TODO mods should be already in flipscores, temporary creating it, very fragile
-  out$summary_table <- cbind(model = sub("^(mod[0-9]+).*", "\\1", rownames(out$summary_table)), out$summary_table)
 
   old_class <- class(out)
   
@@ -132,6 +129,7 @@ pima <- function(mods, tested_coeffs = NULL, n_flips = 5000, method = c("maxT", 
   join_flipscores_args <- .get_fn_args(flipscores::flipscores, 
                                        new.args = flipscores_args, 
                                        exclude = c("...", "mods"))
+  
   info <- .get_info_models(out$objects, extra)
   out$info <- info$info
   out$tested_coeffs <- tested_coeffs
